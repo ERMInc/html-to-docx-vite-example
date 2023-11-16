eabb26a0cf851c345813a5100445626495bd7e9c
Saurabh Harwande <saurabh.harwande@erm.com>
2023-11-16 16:20:33 +0530

    Add vite projects with react and vue

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import HTMLtoDOCX from 'html-to-docx'
+
+const fileBuffer = await HTMLtoDOCX("<html></html>", null, {footer:true});
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
